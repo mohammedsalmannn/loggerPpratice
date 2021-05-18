14ba1c7f7849cbeb86b31e28d639e4d8c12b3a17
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-18 16:26:57 +0530

    Added: Logger_Function_Program

---
+++ src/main/java/com_bridgelabz/logger_pratice/App.java
@@ -0,0 +1,13 @@
+package com_bridgelabz.logger_pratice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
